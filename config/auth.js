7ba42a5800d52c489bae3fdb9e5c89dc1502eb9d
wen <wen@wens-MacBook-Pro.local>
2017-04-15 00:16:39 -0500

    changes

--- config/auth.js
+++ config/auth.js
@@ -9,9 +9,6 @@ module.exports = {
 		'clientID': '',
         'clientSecret': '',
  		'callbackURL': 'http://localhost:3000/auth/google/callback'
-	},
-	'foursquare' : {
-		'clientID': 'B0SVSXBWCRVOHSUCP4OFG0OIJFSHGBMLQFKJDRBZQBO3XYPD',
-        'clientSecret': 'OU15FEKKKPAKV4JWOOGDY3WNTMRUDHVHJL1QFXC1QGPPVO20',
 	}
+	
 }
