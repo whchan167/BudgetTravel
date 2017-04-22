adede57238c259e021625ac0b33d7b1b8a4734df
wen <wen@wens-mbp.attlocal.net>
2017-04-22 07:25:29 -0500

    changes

--- config/auth.js
+++ config/auth.js
@@ -1,17 +1,13 @@
 module.exports = {
 	'facebookAuth' : {
-		'clientID': '796929340454925',
-        'clientSecret': '4ef444af73e244c04fcb44488cfd001a',
+		'clientID': '',
+        'clientSecret': '',
 		'callbackURL': 'https://polar-bastion-63233.herokuapp.com/login/facebook/return'
 	},
 
 	'googleAuth' : {
-		'clientID': '821301834164-8vdo7f3vi3pqhih24ajsej77cjh7d1ft.apps.googleusercontent.com',
-        'clientSecret': '_cqI69s3EbnYLi89BikXPVKb',
+		'clientID': '',
+        'clientSecret': '',
  		'callbackURL': 'https://polar-bastion-63233.herokuapp.com/auth/google/callback'
-	},
-	'foursquare' : {
-		'clientID': 'B0SVSXBWCRVOHSUCP4OFG0OIJFSHGBMLQFKJDRBZQBO3XYPD',
-        'clientSecret': 'OU15FEKKKPAKV4JWOOGDY3WNTMRUDHVHJL1QFXC1QGPPVO20',
 	}
 }
